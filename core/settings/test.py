ab2c2f9c929c2c9eb6644f6bdbdbff0e0d2a0150
tinh2406 <nguyenquoctinh24062002@gmail.com>
2024-10-03 18:15:36 +0700

    feat(test): add test case for auth api

--- core/settings/test.py
+++ core/settings/test.py
@@ -2,3 +2,12 @@ from .base import *  # noqa: F403
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+
+# Đọc biến môi trường
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',  # Sử dụng SQLite cho tests
+        'NAME': (BASE_DIR, 'test_db.sqlite3'),  # Đặt tên cho DB tạm
+    }
+}
